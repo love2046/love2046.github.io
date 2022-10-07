ae8918782c0e50f19c221f2b378706be3cbd4995
k <k@k.k>
2022-10-08 00:55:42 +0800

    change mk2

--- a.py
+++ a.py
@@ -1,9 +1,13 @@
-import markdown
+import markdown2
 text='''
 #this is header
 ##sub title
 hello world
 test
 '''
-html=markdown.markdown(text)
+html=markdown2.markdown(text)
 print(html)
+with open("index.html","w") as f:
+    f.write(html)
+
+
